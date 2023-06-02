74f1a5af3b5da71ef53a1a3ad239e528691acdfb
fureszb <115637991+fureszb@users.noreply.github.com>
2023-06-02 11:43:14 +0200

    Update SzínPaletta.js

--- SzínPaletta.js
+++ SzínPaletta.js
@@ -4,45 +4,44 @@ import { KEPEKLISTA } from "./kepek.js";
 import KepMegjelenit from "./KepMegjelenít.js";
 import Pixel from "./tablaGen.js";
 $(function () {
-  //const painter = new Painter('canvas');
-  let szuloElem = $(".tarolo");
-  let kokckaElem = $(".kockak");
-  let kepElem = $(".kepek");
-  let aktSzin = "red";
-  let kepWidth = 0;
-  for (let index = 0; index < LISTA.length; index++) {
-    new Szin(index, LISTA[index], szuloElem);
-  }
-  $(window).on("kepSzelesseg", (event) => {
-    kepWidth = event.detail;
-    console.log(kepWidth);
-    for (let index = 0; index < kepWidth; index++) {
-      new Pixel(kokckaElem, index);
-    }
-  });
+     //const painter = new Painter('canvas');
+     let szuloElem = $(".tarolo")
+     let kokckaElem = $(".kockak");
+     let kepElem = $(".kepek");
+     let aktSzin = "red";
+     for (let index = 0; index < LISTA.length; index++) {
+          new Szin(index, LISTA[index], szuloElem);
+     }
+     for (let index = 0; index < 1240; index++) {
+          new Pixel(kokckaElem, index);
 
-  $(window).on("szinBeallit", (event) => {
-    let aktElem = event.detail;
-    console.log(aktSzin);
-    aktElem.setSzin(aktSzin);
-    //  ko.setszin(aktElem.getSzin());
-  });
+     }
+     $(window).on("szinBeallit", (event) => {
+          let aktElem = event.detail;
+          console.log(aktSzin);
+          aktElem.setSzin(aktSzin);
+          //  ko.setszin(aktElem.getSzin());
 
-  $(window).on("szinValasztas", (event) => {
-    aktSzin = event.detail;
-    console.log(aktSzin);
-    //  ko.setszin(aktElem.getSzin());
-  });
-  for (let index = 0; index < KEPEKLISTA.length; index++) {
-    let kep = new KepMegjelenit(
-      kepElem,
-      KEPEKLISTA[index],
-      index,
-      kokckaElem,
-      szuloElem
-    );
-  }
+     });
+     $(window).on("huzasBeallit", (event) => {
+          let aktElem = event.detail;
+          console.log(aktSzin);
+          aktElem.setSzin(aktSzin);
+          //  ko.setszin(aktElem.getSzin());
 
-  /* const image = new Kep();
-   image.getWidth();*/
+     });
+
+     $(window).on("szinValasztas", (event) => {
+          aktSzin = event.detail;
+          console.log(aktSzin);
+          //  ko.setszin(aktElem.getSzin());
+
+     });
+     for (let index = 0; index < KEPEKLISTA.length; index++) {
+          let kep = new KepMegjelenit(kepElem, KEPEKLISTA[index], index, kokckaElem, szuloElem);
+     }
+
+     /* const image = new Kep();
+      image.getWidth();*/
+
 });
